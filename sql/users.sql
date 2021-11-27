91478524b471247baa3f019ceb4b43d9009fe044
Jo Exonar <jo.exonar@gmail.com>
2021-11-27 00:37:52 -0800

    start a sql loader script for testing

---
+++ sql/users.sql
@@ -0,0 +1,27 @@
+-- Jojo
+
+INSERT INTO USERS (avid) VALUES ('');
+INSERT INTO OWNERS (avid, ower) VALUES ('', '');
+INSERT INTO OWNERS (avid, ower) VALUES ('', '');
+
+INSERT INTO locations (avid, location) VALUES ('', '');
+INSERT INTO locations (avid, location) VALUES ('', '');
+INSERT INTO locations (avid, location, dwell, per) VALUES ('', '');
+
+-- Megan
+
+INSERT INTO USERS (avid) VALUES ('');
+INSERT INTO OWNERS (avid, ower) VALUES ('', '');
+
+INSERT INTO locations (avid, location) VALUES ('', '');
+INSERT INTO locations (avid, location, dwell, per) VALUES ('', '');
+INSERT INTO locations (avid, location) VALUES ('', '');
+
+-- Gwen
+
+INSERT INTO USERS (avid) VALUES ('');
+INSERT INTO OWNERS (avid, ower) VALUES ('', '');
+
+INSERT INTO locations (avid, location) VALUES ('', '');
+INSERT INTO locations (avid, location) VALUES ('', '');
+INSERT INTO locations (avid, location) VALUES ('', '');
