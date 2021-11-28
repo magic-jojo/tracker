e7cba9f5733663684d675c20ad622e955c7b7b9a
Jo Exonar <jo.exonar@gmail.com>
2021-11-27 21:53:08 -0800

    Implement the travel function: finally getting to the meat of this applicaion.

--- __init__.py
+++ __init__.py
@@ -1,8 +1,9 @@
 from flask import Flask, request
 from psycopg2 import connect
 from json import dumps
+from re import search
+from urllib.parse import unquote
 import pendulum
-
 
 #>>> from psycopg2 import connect
 #>>> with connect(dbname='tracker', user='jojo') as conn:
@@ -26,6 +27,50 @@ def jsonconvert(o):
 app = Flask(__name__)
 
 # Read-side API:
+
+@app.route('/travel/<avid>', methods = ['POST'])
+def travel(avid, state=True):
+  app.logger.info(f"travel({avid})")
+  print(f"travel({avid})")
+
+  try:
+    with connect(dbname='tracker', user='jojo') as conn:
+      with conn.cursor() as cursor:
+        cursor.execute('SELECT expires, recovers, travel, recover FROM users WHERE avid = %s', [avid,])
+        row = cursor.fetchone()
+        expires = pendulum.instance(row[0], 'local')
+        now = pendulum.now()
+        print(f"travel({avid}): travel expires/d: {expires} now: {now}")
+        if now < expires:
+          # We are IN the travel window
+          print(f"travel: in travel time")
+          return {avid: True}
+        recovers = pendulum.instance(row[1], 'local')
+        print(f"travel({avid}): travel recovers/ed: {recovers} now: {now}")
+        if recovers < now:
+          print("We are OUT OF the recovery window")
+          travel = int(row[2])
+          print(f"Travel minutes: {travel}")
+          recover = int(row[3])
+          print(f"Recover minutes: {recover}")
+          print(f"Now: {now.naive()}")
+          expires = now.add(minutes = travel).naive()
+          print(f"New expiration: {expires}")
+          recovers = now.add(minutes = (travel + recover)).naive()
+          print(f"New recovers: {recovers}")
+          print(f"UPDATE users SET expires = {expires}, recovers = {recovers} WHERE avid = {avid}")
+          cursor.execute(
+            'UPDATE users SET expires = %s, recovers = %s WHERE avid = %s',
+            [expires, recovers, avid])
+          return {avid: True}
+        else:
+          # IN the recovery window, so no
+          print(f"travel: not recovered until {recovers}")
+        return {avid: False}
+  except Exception as e:
+    print(f"Shit! {e}") 
+    return str(e), 500
+
 
 # Write-side API:
 
@@ -77,12 +122,12 @@ def lockout(avid, state=True):
     return str(e), 500
 
 
-@app.route('/travel/<avid>', methods = ['POST'])
-@app.route('/travel/<avid>/<away>', methods = ['POST'])
-@app.route('/travel/<avid>/<away>/<recover>', methods = ['POST'])
-def travel(avid, away=0, recover=0):
-  app.logger.info(f"travel({avid}, {away}, {recover})")
-  print(f"travel({avid}, {away}, {recover})")
+@app.route('/settravel/<avid>', methods = ['POST'])
+@app.route('/settravel/<avid>/<away>', methods = ['POST'])
+@app.route('/settravel/<avid>/<away>/<recover>', methods = ['POST'])
+def settravel(avid, away=0, recover=0):
+  app.logger.info(f"settravel({avid}, {away}, {recover})")
+  print(f"settravel({avid}, {away}, {recover})")
 
   tt = int(away)
   rt = int(recover)
@@ -92,8 +137,8 @@ def travel(avid, away=0, recover=0):
   elif rt < tt:
     rt = tt
 
-  app.logger.info(f"fixed travel({avid}, {tt}, {rt})")
-  print(f"fixed travel({avid}, {tt}, {rt})")
+  app.logger.info(f"fixed settravel({avid}, {tt}, {rt})")
+  print(f"fixed settravel({avid}, {tt}, {rt})")
 
   try:
     with connect(dbname='tracker', user='jojo') as conn:
@@ -139,6 +184,32 @@ def delowner(avid, owner):
     return str(e), 500
 
 
+@app.route('/sethome/<avid>/<region>/<x>/<y>/<z>', methods = ['POST'])
+def sethome(avid, region, x, y, z):
+  home = f"{region}/{x}/{y}/{z}"
+  app.logger.info(f"sethome({avid}, {home})")
+  print(f"sethome({avid}, {home})")
+
+  try:
+    with connect(dbname='tracker', user='jojo') as conn:
+      with conn.cursor() as cursor:
+        cursor.execute(
+          'UPDATE users SET home = %s WHERE avid = %s', 
+          [home, avid])
+        # unencode the region for the Locations table, just in case
+        region = unquote(region)
+        print(f"Home {home} region is {region}")
+        cursor.execute(
+          '''INSERT INTO locations (avid, location, dwell, per) VALUES (%s, %s, 0, 0)
+              ON CONFLICT (avid, location) DO UPDATE SET dwell = 0, per = 0 ''',
+              [avid, region]
+        )
+        return {avid: home}
+  except Exception as e:
+    print(f"Frogs! {e}") 
+    return str(e), 500
+
+
 # Our one read-write API, which is still a GET 
 # even though it can create an object
 
@@ -150,7 +221,7 @@ def get(avid):
   try:
     with connect(dbname='tracker', user='jojo') as conn:
       with conn.cursor() as cursor:
-        cursor.execute('SELECT locked, tracking, lockout, travel, recover FROM users WHERE avid = %s', [avid, ])
+        cursor.execute('SELECT locked, tracking, lockout, travel, recover, home FROM users WHERE avid = %s', [avid, ])
         rows = cursor.fetchone()
         # print(rows)
         if rows:
@@ -159,6 +230,7 @@ def get(avid):
                     'lockout': rows[2],
                     'travel': rows[3],
                     'recover': rows[4],
+                    'home': rows[5],
                     'owners': [],
                     'locations': []}
           cursor.execute('SELECT owner FROM owners WHERE avid = %s', [avid, ])
